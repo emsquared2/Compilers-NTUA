ef791d11cc3db92a77f32d2448f8f8400d30fba6
Manos <manos.emmanouilidis05@gmail.com>
2023-11-03 21:32:03 +0200

    Semantic - Initial commit

---
+++ semantic/ast.hpp
@@ -0,0 +1,906 @@
+#ifndef __AST_HPP__
+#define __AST_HPP__
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <vector>
+#include <variant>
+
+extern std::map<std::string, int> globals;
+
+// enum Type
+// {
+//     TYPE_INT,
+//     TYPE_BOOL
+// };
+
+// struct ExprList
+// {
+//     std::vector<Expr *> expr_list;
+// };
+
+class FuncDef;
+class LocalDef;
+class Local;
+
+class AST
+{
+public:
+    virtual ~AST() {}
+    virtual void printOn(std::ostream &out) const = 0;
+};
+
+inline std::ostream &operator<<(std::ostream &out, const AST &t)
+{
+    t.printOn(out);
+    return out;
+}
+
+class Expr : public AST
+{
+public:
+    virtual int eval() const = 0;
+};
+
+class Cond : public AST
+{
+public:
+    virtual int eval() const = 0;
+};
+
+class Stmt : public AST
+{
+public:
+    virtual void run() const = 0;
+};
+
+class LValue : public Expr
+{
+public:
+    virtual int eval() const = 0;
+    virtual std::string getName() const {};
+};
+
+class Const : public Expr
+{
+public:
+    Const(int n) : num(n) {}
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Const(" << num << ")";
+    }
+    virtual int eval() const override
+    {
+        return num;
+    }
+
+private:
+    int num;
+};
+
+class ConstChar : public Expr
+{
+public:
+    ConstChar(char v) : var(v) {}
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "ConstChar(" << var << ")";
+    }
+    virtual int eval() const override
+    {
+        // return var;
+    }
+
+private:
+    char var;
+};
+
+class ConstStr : public LValue
+{
+public:
+    ConstStr(char *s) : var(s) {}
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "ConstStr(" << var << ")";
+    }
+    virtual const char *getStr()
+    {
+        return var;
+    }
+    virtual std::string getName() const override
+    {
+        return std::string(var);
+    }
+    virtual int eval() const override
+    {
+    }
+
+private:
+    const char *var;
+};
+
+class Id : public LValue
+{
+public:
+    Id(char *s) : var(s) {}
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Id(" << var << ")";
+    }
+    virtual std::string getId()
+    {
+        return var;
+    }
+    virtual std::string getName() const override
+    {
+        return var;
+    }
+    virtual int eval() const override
+    {
+    }
+
+private:
+    char *var;
+};
+
+class IdList : public AST
+{
+public:
+    IdList() : idlist() {}
+    ~IdList()
+    {
+        for (Id *id : idlist)
+        {
+            delete id;
+        }
+    }
+    void append(Id *id)
+    {
+        idlist.push_back(id);
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "IdList(";
+        bool first = true;
+        // for (Id *id : idlist)
+        for (auto id = idlist.rbegin(); id != idlist.rend(); ++id)
+        {
+            if (!first)
+                out << ", ";
+            first = false;
+            out << **id;
+        }
+        out << ")";
+    }
+
+private:
+    std::vector<Id *> idlist;
+};
+
+class ArrayDim : public AST
+{
+public:
+    ArrayDim() : dim() {}
+    ~ArrayDim()
+    {
+        for (Const *num : dim)
+        {
+            delete num;
+        }
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        for (Const *num : dim)
+        {
+            out << "[" << *num << "]";
+        }
+    }
+    bool isEmpty()
+    {
+        return dim.empty();
+    }
+    void append(Const *num)
+    {
+        dim.push_back(num);
+    }
+
+private:
+    std::vector<Const *> dim;
+};
+
+class ArrayElem : public LValue
+{
+public:
+    ArrayElem(LValue *l, Expr *e) : left(l), expr(e) {}
+    ~ArrayElem()
+    {
+        delete left;
+        delete expr;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "ArrayElem(" << *left << "[" << *expr << "]";
+    }
+    virtual std::string getArrayElem()
+    {
+        return left->getName() + "[" + std::to_string(expr->eval()) + "]";
+    }
+    virtual int eval() const override
+    {
+    }
+
+private:
+    LValue *left;
+    Expr *expr;
+};
+
+class ExprList : public AST
+{
+public:
+    ExprList() : expr_list() {}
+    ~ExprList()
+    {
+        for (Expr *expr : expr_list)
+        {
+            delete expr;
+        }
+    }
+    void append(Expr *e) { expr_list.push_back(e); }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "ExprList(";
+        bool first = true;
+        // for (Expr *e : expr_list)
+        for (auto e = expr_list.rbegin(); e != expr_list.rend(); ++e)
+        {
+            if (!first)
+                out << ", ";
+            first = false;
+            out << **e;
+        }
+        out << ")";
+    }
+
+private:
+    std::vector<Expr *> expr_list;
+};
+
+class StmtList : public AST
+{
+public:
+    StmtList() : stmt_list() {}
+    ~StmtList()
+    {
+        for (Stmt *stmt : stmt_list)
+        {
+            delete stmt;
+        }
+    }
+    void append(Stmt *s) { stmt_list.push_back(s); }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "StmtList(";
+        bool first = true;
+        // for (Stmt *s : stmt_list)
+        for (auto s = stmt_list.rbegin(); s != stmt_list.rend(); ++s)
+        {
+            if (!first)
+                out << ", ";
+            first = false;
+            out << **s;
+        }
+        out << ")";
+    }
+
+private:
+    std::vector<Stmt *> stmt_list;
+};
+
+class Block : public Stmt
+{
+public:
+    Block() : stmt_list() {}
+    ~Block()
+    {
+        for (Stmt *s : stmt_list)
+            delete s;
+    }
+    void append(Stmt *s) { stmt_list.push_back(s); }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Block(";
+        bool first = true;
+        // for (Stmt *s : stmt_list)
+        for (auto s = stmt_list.rbegin(); s != stmt_list.rend(); ++s)
+        {
+            if (!first)
+                out << ", ";
+            first = false;
+            out << **s;
+        }
+        out << ")";
+    }
+    virtual void run() const override
+    {
+        // for (Stmt *s : stmt_list)
+        //     s->run();
+    }
+    std::vector<Stmt *> getList()
+    {
+        return stmt_list;
+    }
+
+private:
+    std::vector<Stmt *> stmt_list;
+};
+
+class DataType : public AST
+{
+public:
+    DataType(char *t) : type(t) {}
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "DataType(" << type << ")";
+    }
+
+private:
+    char *type;
+};
+
+class Type : public AST
+{
+public:
+    Type(DataType *t, ArrayDim *d) : type(t), dim(d) {}
+    ~Type()
+    {
+        delete type;
+        delete dim;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Type(" << *type << *dim << ")";
+    }
+
+private:
+    DataType *type;
+    ArrayDim *dim;
+};
+
+class RetType : public AST
+{
+public:
+    RetType(DataType *t = nullptr) : dtype(t) {}
+    ~RetType() { delete dtype; }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "RetType(";
+        if (dtype == nullptr)
+            out << "nothing)";
+        else
+            out << *dtype << ")";
+    }
+
+private:
+    DataType *dtype;
+};
+
+class FParType : public AST
+{
+public:
+    FParType(DataType *t, ArrayDim *d, bool dl = false) : type(t), dim(d), dimlen(dl) {}
+    ~FParType()
+    {
+        delete type;
+        delete dim;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "FParType(" << *type;
+        if (dimlen)
+            out << "[ ]";
+        out << *dim << ")";
+    }
+
+private:
+    DataType *type;
+    ArrayDim *dim;
+    bool dimlen;
+};
+
+class FParam : public AST
+{
+public:
+    FParam(IdList *idl, FParType *t, bool ref = false) : idlist(idl), fpartype(t), ref(ref) {}
+    ~FParam() { delete idlist; }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "FParam(";
+        if (ref)
+            out << "ref ";
+        out << *idlist << " : " << *fpartype << ")";
+    }
+
+private:
+    IdList *idlist;
+    FParType *fpartype;
+    bool ref;
+};
+
+class ParamList : public AST
+{
+public:
+    ParamList() : params() {}
+    ~ParamList()
+    {
+        for (FParam *param : params)
+        {
+            delete param;
+        }
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        bool first = true;
+        out << "ParamList(";
+        // for (FParam *param : params)
+        for (auto param = params.rbegin(); param != params.rend(); ++param)
+        {
+            if (!first)
+                out << "; ";
+            first = false;
+            out << **param;
+        }
+        out << ")";
+    }
+    void append(FParam *param)
+    {
+        params.push_back(param);
+    }
+
+private:
+    std::vector<FParam *> params;
+};
+
+class Decl : public AST
+{
+public:
+    Decl(IdList *idl, Type *t) : idlist(idl), type(t) {}
+    ~Decl()
+    {
+        delete idlist;
+        delete type;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Decl(var " << *idlist << " : " << *type << ")";
+    }
+
+private:
+    IdList *idlist;
+    Type *type;
+};
+
+class Header : public AST
+{
+public:
+    Header(Id *id, RetType *t, ParamList *pl = nullptr) : id(id), type(t), paramlist(pl) {}
+    ~Header()
+    {
+        delete id;
+        delete paramlist;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Header(fun " << *id << "(";
+        if (paramlist != nullptr)
+            out << *paramlist;
+        out << ") : " << *type;
+    }
+
+private:
+    Id *id;
+    RetType *type;
+    ParamList *paramlist;
+};
+
+class FuncDecl : public AST
+{
+public:
+    FuncDecl(Header *h) : header(h) {}
+    ~FuncDecl() { delete header; }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "FuncDecl(" << *header << ")";
+    }
+
+private:
+    Header *header;
+};
+
+// class FuncDef;
+
+// class Local : public AST
+// {
+// public:
+//     Local(FuncDef *fdef = nullptr, FuncDecl *fdecl = nullptr, Decl *d = nullptr) : funcdef(fdef), funcdecl(fdecl), decl(d) {}
+//     ~Local()
+//     {
+//         delete funcdef;
+//         delete funcdecl;
+//         delete decl;
+//     }
+//     virtual void printOn(std::ostream &out) const override
+//     {
+//         out << "Local(";
+//         if (funcdef != nullptr)
+//         {
+//             out << funcdef;
+//         }
+//         else if (funcdecl != nullptr)
+//         {
+//             out << *funcdecl;
+//         }
+//         else if (decl != nullptr)
+//         {
+//             out << *decl;
+//         }
+//         out << ")";
+//     }
+
+// private:
+//     FuncDef *funcdef;
+//     FuncDecl *funcdecl;
+//     Decl *decl;
+// };
+
+// class LocalDef : public AST
+// {
+// public:
+//     LocalDef() : locals() {}
+//     ~LocalDef()
+//     {
+//         for (Local *l : locals)
+//         {
+//             delete l;
+//         }
+//     }
+//     virtual void printOn(std::ostream &out) const override
+//     {
+//         out << "LocalDef(";
+//         bool first = true;
+//         for (auto l = locals.rbegin(); l != locals.rend(); ++l)
+//         {
+//             if (!first)
+//                 out << ", ";
+//             out << *l;
+//             first = false;
+//         }
+//         out << ")";
+//     }
+//     void append(Local *l)
+//     {
+//         locals.push_back(l);
+//     }
+
+// private:
+//     std::vector<Local *> locals;
+// };
+
+// class FuncDef : public AST
+// {
+// public:
+//     FuncDef(Header *h, LocalDef *ld, Stmt *s) : header(h), localdef(ld), stmt(s) {}
+//     ~FuncDef()
+//     {
+//         delete header;
+//         delete localdef;
+//         delete stmt;
+//     }
+//     virtual void printOn(std::ostream &out) const override
+//     {
+//         out << "FuncDef(";
+//         out << *header << *localdef << *stmt;
+//         out << ")";
+//     }
+
+// private:
+//     Header *header;
+//     LocalDef *localdef;
+//     // Block *block;
+//     Stmt *stmt;
+// };
+
+class Assign : public Stmt
+{
+public:
+    Assign(LValue *l, Expr *e) : l_value(l), expr(e) {}
+    ~Assign()
+    {
+        delete l_value;
+        delete expr;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Assign(" << *l_value << " <- " << *expr << ")";
+    }
+    virtual void run() const override
+    {
+        // globals[l_value->getName()] = expr->eval();
+    }
+
+private:
+    LValue *l_value;
+    Expr *expr;
+};
+
+class CallExpr : public Expr
+{
+public:
+    CallExpr(Id *id, ExprList *e_list = nullptr) : id(id), expr_list(e_list) {}
+    ~CallExpr()
+    {
+        delete id;
+        delete expr_list;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "CallExpr(" << *id << "(";
+        if (expr_list != nullptr)
+            out << *expr_list;
+        out << "))";
+    }
+    // TODO: Implement how a function is run.
+    virtual int eval() const override
+    {
+    }
+
+private:
+    Id *id;
+    ExprList *expr_list;
+};
+
+class CallStmt : public Stmt
+{
+public:
+    CallStmt(Id *id, ExprList *e_list = nullptr) : id(id), expr_list(e_list) {}
+    ~CallStmt()
+    {
+        delete id;
+        delete expr_list;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "CallStmt(" << *id << "(";
+        if (expr_list != nullptr)
+            out << *expr_list;
+        out << "))";
+    }
+    // TODO: Implement how a function is run.
+    // virtual int eval() const override
+    // {
+    // }
+    virtual void run() const override
+    {
+    }
+
+private:
+    Id *id;
+    ExprList *expr_list;
+};
+
+class UnOp : public Expr
+{
+public:
+    UnOp(char *s, Expr *e) : op(s), right(e) {}
+    ~UnOp()
+    {
+        delete right;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << op << "(" << *right << ")";
+    }
+    virtual int eval() const override
+    {
+        if (op == "+")
+        {
+            return right->eval();
+        }
+        else if (op == "-")
+        {
+            return -right->eval();
+        }
+    }
+
+private:
+    char *op;
+    Expr *right;
+};
+
+class BinOp : public Expr, public Cond
+{
+public:
+    BinOp(Expr *l, char *s, Expr *r) : left(l), op(s), right(r) {}
+    ~BinOp()
+    {
+        delete left;
+        delete right;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << op << "(" << *left << ", " << *right << ")";
+    }
+    virtual int eval() const override
+    {
+        if (op == "+")
+        {
+            return left->eval() + right->eval();
+        }
+        else if (op == "-")
+        {
+            return left->eval() - right->eval();
+        }
+        else if (op == "*")
+        {
+            return left->eval() * right->eval();
+        }
+        else if (op == "div")
+        {
+            return left->eval() / right->eval();
+        }
+        else if (op == "mod")
+        {
+            return left->eval() % right->eval();
+        }
+        else if (op == "=")
+        {
+            return left->eval() == right->eval();
+        }
+        else if (op == "#")
+        {
+            return left->eval() != right->eval();
+        }
+        else if (op == "<")
+        {
+            return left->eval() < right->eval();
+        }
+        else if (op == ">")
+        {
+            return left->eval() > right->eval();
+        }
+        else if (op == "<=")
+        {
+            return left->eval() <= right->eval();
+        }
+        else if (op == ">=")
+        {
+            return left->eval() >= right->eval();
+        }
+
+        return 0;
+    }
+
+private:
+    Expr *left;
+    char *op;
+    Expr *right;
+};
+
+class OpCond : public Cond
+{
+public:
+    OpCond(Cond *l, char *s, Cond *r) : left(l), op(s), right(r) {}
+    OpCond(char *s, Cond *r) : left(), op(s), right(r) {}
+    ~OpCond()
+    {
+        delete left;
+        delete right;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << op << "(";
+        if (left != nullptr)
+            out << *left << ", ";
+        out << *right << ")";
+    }
+    virtual int eval() const override
+    {
+        if (op == "not")
+        {
+            return !right->eval();
+        }
+        else if (op == "and")
+        {
+            return left->eval() && right->eval();
+        }
+        else if (op == "or")
+        {
+            return left->eval() || right->eval();
+        }
+        return 0;
+    }
+
+private:
+    Cond *left;
+    char *op;
+    Cond *right;
+};
+
+class Return : public Stmt
+{
+public:
+    Return(Expr *e = nullptr) : expr(e) {}
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "Return(";
+        if (expr != nullptr)
+            out << *expr;
+        out << ")";
+    }
+    virtual void run() const override
+    {
+    }
+    int ReturnValue()
+    {
+        return (expr != nullptr) ? expr->eval() : 0;
+    }
+
+private:
+    Expr *expr;
+};
+
+class If : public Stmt
+{
+public:
+    If(Cond *c, Stmt *s1, Stmt *s2 = nullptr) : cond(c), stmt1(s1), stmt2(s2) {}
+    ~If()
+    {
+        delete cond;
+        delete stmt1;
+        delete stmt2;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "If(" << *cond << ", " << *stmt1;
+        if (stmt2 != nullptr)
+            out << " Else " << *stmt2;
+        out << ")";
+    }
+    virtual void run() const override
+    {
+        if (cond->eval())
+            stmt1->run();
+        else if (stmt2 != nullptr)
+            stmt2->run();
+    }
+
+private:
+    Cond *cond;
+    Stmt *stmt1, *stmt2;
+};
+
+class While : public Stmt
+{
+public:
+    While(Cond *c, Stmt *s) : cond(c), stmt(s) {}
+    ~While()
+    {
+        delete cond;
+        delete stmt;
+    }
+    virtual void printOn(std::ostream &out) const override
+    {
+        out << "While(" << *cond << ", " << *stmt << ")";
+    }
+    virtual void run() const override
+    {
+        while (cond->eval())
+            stmt->run();
+    }
+
+private:
+    Cond *cond;
+    Stmt *stmt;
+};
+
+#endif
---
+++ semantic/parser.y
@@ -0,0 +1,288 @@
+%{
+    #include <cstdio>
+    #include <stdio.h>
+    #include <vector>
+    #include "lexer.hpp"
+    #include "ast.hpp"
+    #include "localdef.hpp"
+    #include "funcdef.hpp"
+    #include "local.hpp"
+
+    #define YYDEBUG 1
+
+%}
+
+
+%token T_and        "and"
+%token T_bool       "bool"
+%token<str> T_char    "char"
+%token T_div        "div"
+%token T_do         "do"
+%token T_else       "else"
+%token T_fun        "fun"
+%token T_if         "if"
+%token<str> T_int   "int"
+%token T_mod        "mod"
+%token T_not        "not"
+%token T_nothing    "nothing"
+%token T_or         "or"
+%token T_ref        "ref"
+%token T_return     "return"
+%token T_then       "then"
+%token T_var        "var"
+%token T_while      "while"
+
+         
+
+%expect 1
+%define parse.error /*detailed*/ verbose
+%verbose
+%define parse.trace
+
+%union {
+    Expr *expr;
+    LValue *lvalue;
+    Block *block;
+    ExprList *exprlist;
+    StmtList *stmtlist;
+    Cond *cond;
+    Stmt *stmt;
+    Decl *decl;
+    FParam *fparam;
+    Header *header;
+    Local *local;
+    FuncDecl *funcdecl;
+    FuncDef *funcdef;
+    LocalDef *localdef;
+    IdList *idlist;
+    ParamList *paramlist;
+    ArrayDim *arraydim;
+    Type *type; // as we deal with more  types might change 
+    DataType *datatype;
+    RetType *rettype;
+    FParType *fpartype;
+    Id *id;
+    Const *cons;
+    ConstChar *constchar;
+    ConstStr *conststr;
+    char *op, c, *str;
+    //std::string str;
+    int num;
+}
+
+//%token<id> T_id
+%token<str> T_id
+//%token<cons> T_const
+%token<num> T_const      
+%token<c> T_const_char 
+%token<str> T_const_str 
+/* %token<constchar> T_const_char 
+%token<conststr> T_const_str   */
+%token T_ge         ">="     
+%token T_le         "<="
+%token<op> T_assign     "<-"
+
+%left<op> T_or
+%left<op> T_and
+%nonassoc<op> T_not
+%nonassoc<op> '=' '#' '>' '<' T_ge T_le
+%left<op> '+' '-'
+%left<op> '*' T_div T_mod
+
+%type<block> stmt-list block program
+%type<funcdef> func-def /*program*/
+%type<exprlist> expr-list
+%type<expr> expr func-call-expr/*l-value*/ /*func-call*/
+%type<lvalue> l-value
+%type<decl> var-def
+%type<cond> cond
+//%type<stmtlist> stmt-list
+%type<stmt> stmt /*func-call */ func-call-stmt
+%type<fparam> fpar-def
+%type<header> header
+%type<local> local
+%type<funcdecl> func-decl
+%type<localdef> local-def
+%type<idlist> id-extended
+%type<paramlist> fpar-def-extended
+%type<arraydim> bracket-extended
+%type<type> type;
+%type<datatype> data-type
+%type<rettype> ret-type
+%type<fpartype> fpar-type
+%type<cons> const
+%type<id> id;
+
+
+%%
+
+program:
+    /* nothing */   { std::cout << "Empty program" << std::endl; }
+    | func-def  {  std::cout << "AST" << *$1 << std::endl;
+        //$$ = $1; 
+    }
+;
+
+func-def:
+    header local-def block      { $$ = new FuncDef($1, $2, $3); }
+;
+
+header: 
+    "fun" id '(' fpar-def fpar-def-extended ')' ':' ret-type  { $5->append($4); $$ = new Header($2, $8, $5); }
+    | "fun" id '(' ')' ':' ret-type                           { $$ = new Header($2, $6); }
+;
+
+fpar-def-extended:
+    /* nothing */                       { $$ = new ParamList();}
+    | ';' fpar-def fpar-def-extended    { $3->append($2); $$ = $3;}
+;
+
+fpar-def:
+    "ref" id id-extended ':' fpar-type { $3->append($2); $$ = new FParam($3, $5, true);}
+    | id id-extended ':' fpar-type     { $2->append($1); $$ = new FParam($2, $4);}
+;
+
+id-extended:
+    /* nothing */               { $$ = new IdList(); }
+    | ',' id id-extended        { $3->append($2); $$ = $3; }
+;
+
+fpar-type: // use $2->isEmpty() to identify simple params (int / char) from arrays
+    data-type bracket-extended              { $$ = new FParType($1, $2); }
+    | data-type '[' ']' bracket-extended    { $$ = new FParType($1, $4, true); }
+;
+
+bracket-extended:
+    /* nothing */                                 { $$ = new ArrayDim(); } // use
+    | '[' /*T_const*/ const ']' bracket-extended  { $4->append($2); $$ = $4;}
+; 
+
+data-type:
+    "int"           /* { $$ = TYPE_INT;  } */ { $$ = new DataType($1);}
+    | "char"        /* { $$ = TYPE_BOOL; } */ { $$ = new DataType($1);}
+;
+
+ret-type:
+    data-type       { $$ = new RetType($1); }
+    | "nothing"     { $$ = new RetType();   }
+;
+
+local-def:
+    /* nothing*/                { $$ = new LocalDef(); }
+    | local local-def           { $2->append($1); $$ = $2; }
+    /* | func-def local-def        { $2->append($1); $$ = $2; }
+    | func-decl local-def       { $2->append($1); $$ = $2; }
+    | var-def local-def         { $2->append($1); $$ = $2; } */
+;
+
+local:
+    func-def                    { $$ = new Local($1); }
+    | func-decl                 { $$ = new Local($1); }
+    | var-def                   { $$ = new Local($1); }
+;
+
+func-decl:
+    header ';'  { $$ = new FuncDecl($1); }
+;
+
+var-def:
+    "var" id id-extended ':' type ';'     { $3->append($2); $$ = new Decl($3, $5); }
+;
+
+type:
+    data-type bracket-extended      { $$ = new Type($1, $2); }
+;
+
+block:
+    '{' stmt-list '}'   { $$ = $2; }
+;
+
+stmt-list: 
+    /* nothing */       { $$ = new Block(); }
+    | stmt stmt-list    { $2->append($1); $$ = $2;}
+;
+
+stmt:
+    ';'                                     { $$ = nullptr; }
+    | l-value T_assign expr ';'             { $$ = new Assign($1, $3); }
+    | block                                 { $$ = $1; }
+    | func-call-stmt ';'                    { $$ = $1; }
+    | "if" cond "then" stmt                 { $$ = new If($2, $4); }
+    | "if" cond "then" stmt "else" stmt     { $$ = new If($2, $4, $6); }
+    | "while" cond "do" stmt                { $$ = new While($2, $4); }
+    | "return" ';'                          { $$ = new Return(); }
+    | "return" expr ';'                     { $$ = new Return($2); }    
+;
+
+l-value:                   
+    id/*T_id*/              { $$ = $1;}
+    | T_const_str           { $$ = new ConstStr($1);}
+    | l-value '[' expr ']'  { $$ = new ArrayElem($1, $3);} 
+;
+
+id :
+    T_id                    { std::cout << "here40" << $1 << std::endl; $$ = new Id($1);}
+;
+// changed all T_id to id to satisfy correct id
+
+expr:
+    //T_const             { $$ = new Const($1); }
+    const               { $$ = $1; }
+    | T_const_char      { $$ = new ConstChar($1); }
+    | l-value           { $$ = $1; }
+    | '(' expr ')'      { $$ = $2; }
+    | func-call-expr    { $$ = $1; } // Check if run function needed.  
+    | '+' expr          { $$ = new UnOp("+", $2); }
+    | '-' expr          { $$ = new UnOp("-", $2); }
+    | expr '+' expr     { $$ = new BinOp($1, "+", $3);}
+    | expr '-' expr     { $$ = new BinOp($1, "-", $3);}
+    | expr '*' expr     { $$ = new BinOp($1, "*", $3);}
+    | expr "div"  expr  { $$ = new BinOp($1, "div", $3);}
+    | expr "mod"  expr  { $$ = new BinOp($1, "mod", $3);}
+;
+
+const: 
+    T_const           { $$ = new Const($1); }
+;
+// changed impl just to have correct type (custom Const class instead of int)
+// Might need further examination ## 
+
+func-call-stmt: 
+    id '(' ')'                    { $$ = new CallStmt($1); }
+    | id '(' expr expr-list ')'   { $4->append($3); $$ = new CallStmt($1, $4); }
+;
+
+func-call-expr: 
+    id '(' ')'                    { $$ = new CallExpr($1); }
+    | id '(' expr expr-list ')'   { $4->append($3); $$ = new CallExpr($1, $4); }
+;
+
+expr-list:
+    /* nothing */           { $$ = new ExprList();}
+    | ',' expr expr-list    { $3->append($2); $$ = $3;}
+;
+
+cond: 
+    '(' cond ')'        { $$ = $2; }
+    | "not" cond        { $$ = new OpCond("not", $2); }
+    | cond "and" cond   { $$ = new OpCond($1, "and", $3); }       
+    | cond "or" cond    { $$ = new OpCond($1, "or", $3); }    
+    | expr '=' expr     { $$ = new BinOp($1, "=", $3);}
+    | expr '#' expr     { $$ = new BinOp($1, "#", $3);}
+    | expr '<' expr     { $$ = new BinOp($1, "<", $3);}
+    | expr '>' expr     { $$ = new BinOp($1, ">", $3);}
+    | expr T_le expr    { $$ = new BinOp($1, "<=", $3);}
+    | expr T_ge expr    { $$ = new BinOp($1, ">=", $3);}
+;
+
+%%
+
+int main() {
+    // #ifdef YYDEBUG
+    //     int yydebug = 1;
+    // #endif
+
+    int result = yyparse();
+    if (result == 0) printf("Success.\n");
+    return result;
+}

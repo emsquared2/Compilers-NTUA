06e4510a19459089d8eaecc160dafbcc91acc684
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-04-21 23:39:41 +0300

    Expressions

---
+++ semantic/expressions.cpp
@@ -0,0 +1,128 @@
+#include "expressions.hpp"
+
+/* ---------------------------------------------------------------------
+   ------------------------------- UnOp --------------------------------
+   --------------------------------------------------------------------- */
+
+UnOp::UnOp(char *s, Expr *e) : op(s), right(e) {}
+
+UnOp::~UnOp()
+{
+    delete right;
+}
+
+void UnOp::printOn(std::ostream &out) const
+{
+    out << op << "(" << *right << ")";
+}
+
+void UnOp::sem()
+{
+    right->type_check(typeInteger);
+    type = typeInteger;
+}
+
+/* ---------------------------------------------------------------------
+   ------------------------------- BinOp -------------------------------
+   --------------------------------------------------------------------- */
+
+BinOp::BinOp(Expr *l, char *s, Expr *r) : left(l), op(s), right(r) {}
+
+BinOp::~BinOp()
+{
+    delete left;
+    delete right;
+}
+
+void BinOp::printOn(std::ostream &out) const
+{
+    out << op << "(" << *left << ", " << *right << ")";
+}
+
+void BinOp::sem()
+{
+    left->type_check(typeInteger);
+    right->type_check(typeInteger);
+    type = typeInteger;
+}
+
+
+/* #####################################################################
+   #####                        CONST TYPES                       #####
+   ##################################################################### */
+
+/* ---------------------------------------------------------------------
+   ------------------------------- Const -------------------------------
+   --------------------------------------------------------------------- */
+
+
+Const::Const(int v) : val(v) {}
+Const::~Const() {}
+void Const::printOn(std::ostream &out) const
+{
+    out << "Const(" << val << ")";
+}
+
+void Const::sem() 
+{ 
+    type = typeInteger;
+}
+
+
+/* ---------------------------------------------------------------------
+   ----------------------------- ConstChar -----------------------------
+   --------------------------------------------------------------------- */
+
+ConstChar::ConstChar(char v) : var(v) {}
+ConstChar::~ConstChar() {}
+void ConstChar::printOn(std::ostream &out) const
+{
+    out << "ConstChar(" << var << ")";
+}
+    // virtual int eval() const override
+    // {
+    //     // return var;
+    // }
+void ConstChar::sem()  
+{
+    type = typeChar;
+}
+
+/* ---------------------------------------------------------------------
+   ------------------------------ Nothing ------------------------------
+   --------------------------------------------------------------------- */
+
+Nothing::Nothing() {}
+Nothing::~Nothing() {}
+void Nothing::printOn(std::ostream &out) const
+{
+    out << "Nothing";
+}
+void Nothing::sem()
+{
+    type = typeVoid;
+}
+
+/* ---------------------------------------------------------------------
+   ---------------------- FUNCTION CALL EXPRESSION ---------------------
+   --------------------------------------------------------------------- */
+
+CallExpr::CallExpr(Id *id, ExprList *e_list = nullptr) : id(id), expr_list(e_list) {}
+CallExpr::~CallExpr()
+{
+    delete id;
+    delete expr_list;
+}
+void CallExpr::printOn(std::ostream &out) const
+{
+    out << "CallExpr(" << *id << "(";
+    if (expr_list != nullptr)
+        out << *expr_list;
+    out << "))";
+}
+void CallExpr::sem()
+{
+    id->sem();
+    expr_list->sem();
+    // Also do smth with type (inherited from class Expr)
+}
---
+++ semantic/expressions.hpp
@@ -0,0 +1,197 @@
+#ifndef __EXPRESSIONS_HPP__
+#define __EXPRESSIONS_HPP__
+
+#pragma once 
+
+#include <vector>
+#include "ASTabstracts.hpp"
+#include "ast.hpp"
+
+
+/* #####################################################################
+   #####                         EXPRESSIONS                       #####
+   ##################################################################### */
+
+
+/* ---------------------------------------------------------------------
+   ------------------------------- UnOp --------------------------------
+   --------------------------------------------------------------------- */
+
+
+class UnOp : public Expr
+{
+public:
+    UnOp(char *s, Expr *e);
+    ~UnOp();
+    virtual void printOn(std::ostream &out) const override;
+    virtual void sem() override;
+    // virtual int eval() const override
+    // {
+    //     if (op == "+")
+    //     {
+    //         return right->eval();
+    //     }
+    //     else if (op == "-")
+    //     {
+    //         return -right->eval();
+    //     }
+    // }
+
+private:
+    const char *op;
+    Expr *right;
+};
+
+/* ---------------------------------------------------------------------
+   ------------------------------- BinOp -------------------------------
+   --------------------------------------------------------------------- */
+
+class BinOp : public Expr
+{
+public:
+    BinOp(Expr *l, char *s, Expr *r);
+    ~BinOp();
+    virtual void printOn(std::ostream &out) const override;
+    virtual void sem() override;
+
+    // virtual int eval() const override
+    // {
+    //     if (op == "+")
+    //     {
+    //         return left->eval() + right->eval();
+    //     }
+    //     else if (op == "-")
+    //     {
+    //         return left->eval() - right->eval();
+    //     }
+    //     else if (op == "*")
+    //     {
+    //         return left->eval() * right->eval();
+    //     }
+    //     else if (op == "div")
+    //     {
+    //         return left->eval() / right->eval();
+    //     }
+    //     else if (op == "mod")
+    //     {
+    //         return left->eval() % right->eval();
+    //     }
+    //     else if (op == "=")
+    //     {
+    //         return left->eval() == right->eval();
+    //     }
+    //     else if (op == "#")
+    //     {
+    //         return left->eval() != right->eval();
+    //     }
+    //     else if (op == "<")
+    //     {
+    //         return left->eval() < right->eval();
+    //     }
+    //     else if (op == ">")
+    //     {
+    //         return left->eval() > right->eval();
+    //     }
+    //     else if (op == "<=")
+    //     {
+    //         return left->eval() <= right->eval();
+    //     }
+    //     else if (op == ">=")
+    //     {
+    //         return left->eval() >= right->eval();
+    //     }
+
+    //     return 0;
+    // }
+
+private:
+    Expr *left;
+    const char *op;
+    Expr *right;
+};
+
+
+/* #####################################################################
+   #####                         CONST TYPES                       #####
+   ##################################################################### */
+
+/* ---------------------------------------------------------------------
+   ------------------------------- Const -------------------------------
+   --------------------------------------------------------------------- */
+
+class Const : public Expr
+{
+public:
+    Const(int val);
+    ~Const();
+    virtual void printOn(std::ostream &out) const override;
+    virtual void sem() override;
+    // virtual int eval() const override
+    // {
+    //     return val;
+    // }
+
+private:
+    const int val;
+};
+
+/* ---------------------------------------------------------------------
+   ----------------------------- ConstChar -----------------------------
+   --------------------------------------------------------------------- */
+
+
+class ConstChar : public Expr
+{
+public:
+    ConstChar(char v);
+    ~ConstChar();
+    virtual void printOn(std::ostream &out) const override;
+    // virtual int eval() const override
+    // {
+    //     // return var;
+    // }
+    virtual void sem() override;
+
+private:
+    char var;
+};
+
+/* ---------------------------------------------------------------------
+   ------------------------------ Nothing ------------------------------
+   --------------------------------------------------------------------- */
+
+class Nothing : public Expr
+{
+public:
+    Nothing();
+    ~Nothing();
+    virtual void printOn(std::ostream &out) const override;
+    virtual void sem() override;
+};
+
+/* ---------------------------------------------------------------------
+   ---------------------- FUNCTION CALL EXPRESSION ---------------------
+   --------------------------------------------------------------------- */
+
+class Id;
+class ExprList;
+
+class CallExpr : public Expr
+{
+public:
+    CallExpr(Id *id, ExprList *e_list);
+    ~CallExpr();
+    virtual void printOn(std::ostream &out) const override;
+    // TODO: Implement how a function is run.
+    // virtual int eval() const override
+    // {
+    // }
+    virtual void sem() override;
+
+private:
+    Id *id;
+    ExprList *expr_list;
+};
+
+
+#endif

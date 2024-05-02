281252cc9418bec8bfd1fd2569f4bdda1433997f
Manos Emmanouilidis <manos.emmanouilidis05@gmail.com>
2024-05-02 19:51:30 +0300

    Implement sem for Return Class

--- semantic/statements.cpp
+++ semantic/statements.cpp
@@ -175,14 +175,35 @@ int Return::ReturnValue()
     // return (expr != nullptr) ? expr->eval() : 0;
 }
 
-void Return::sem()
-{
+void Return::sem() {
     std::cout << "Return Sem..." << std::endl;
-    if (expr != nullptr)
-    {
-        expr->sem();
-        // expr->type_check();
+
+    /* TODO: Get return type.
+     * There are 2 options:
+     *      a) Use a stack that keeps the return types (function decls are nested so you only need to check the last element).
+     *      b) Implement a method in symbol.c in order to get the return type from the last function in scope.
+     */
+
+    SymbolEntry *func = lookupLastFunction();
+
+    Type return_type = func->u.eFunction.resultType;
+
+    // If return type is void, then expr should be nullptr
+    if(equalType(return_type, typeVoid)) {
+        if(expr != nullptr) {
+            SemanticError("Return type should be void");
+        }
     }
+    else if(equalType(return_type, typeInteger) || equalType(return_type, typeChar))
+    {
+        if(expr != nullptr) 
+            expr->type_check(return_type);
+        else SemanticError("Return's expression type does not match return type.");
+    }
+    else {
+        SemanticError("Return type should be int, char or void");
+    }
+
 }
 
 /* ---------------------------------------------------------------------
